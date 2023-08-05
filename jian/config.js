2a92baccb196ce7f4601bf39c091417520dda633
Wulian233 <71213467+Wulian233@users.noreply.github.com>
2023-08-05 15:10:27 +0800

    Add files via upload

---
+++ jian/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'VM汉化组网站状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1418040-e02854077b312589dc334b9e',
+  ],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '返回',
+      url: '../index.html'
+    },
+    {
+      text: '爱发电',
+      url: 'http://afdian.bilibil.eu.org/'
+    },
+    {
+      text: 'B站',
+      url: 'https://b23.tv/dwLeoed'
+    },
+  ],
+};
